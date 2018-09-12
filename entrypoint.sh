2a61c4bac93020de7be6247d36ccd50bf571bbdc
ssuareza <sebastian@typeform.com>
2018-09-12 15:32:49 +0200

    Adding exit 0 to avoid errors.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "${POSTGRES_HOST}":*:*:"${POSTGRES_USER}":"${POSTGRES_PASS}" > ~/.pgpass
 chmod 0600 ~/.pgpass
-cat ~/.pgpass
 psql -h ${POSTGRES_HOST} -U ${POSTGRES_USER} -w postgres -c "$@"
+exit 0
